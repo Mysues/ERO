14f036a2b7249e588b83662bbb3517301f696bb0
syed93uthman <syed_uthman@yahoo.com>
2019-12-02 10:01:11 +0800

    First Commit

---
+++ src/ERO.cpp
@@ -0,0 +1,68 @@
+#include "ERO.h"
+
+ERO::ERO(int *motorPin, int *signalPin, int beeperPin, int speedCtrlPin)
+{
+    motorPinSize = sizeof(_motorPin) / sizeof(int);
+    signalPinSize = sizeof(_signalPin) / sizeof(int);
+    for (int i = 0; i < motorPinSize; i++)
+        _motorPin[i] = motorPin[i];
+
+    for (int i = 0; i < signalPinSize; i++)
+        _signalPin[i] = signalPin[i];
+
+    _beeperPin = beeperPin;
+    _speedCtrlPin = speedCtrlPin;
+}
+
+void ERO::begin()
+{
+    for (int i = 0; i < motorPinSize; i++)
+    {
+        if (i == 2 || i == 5)
+            pinMode(_motorPin[i], OUTPUT);
+    }
+
+    for (int i = 0; i < signalPinSize; i++)
+        pinMode(_signalPin[i], OUTPUT);
+
+    pinMode(_beeperPin, OUTPUT);
+    EROmotor.begin(_motorPin);
+}
+
+void ERO::leftSignal(String mode)
+{
+
+    if (mode.compareTo("ON"))
+        digitalWrite(_signalPin[0], HIGH);
+
+    else if (mode.compareTo("OFF"))
+        digitalWrite(_signalPin[0], LOW);
+
+}
+
+void ERO::rightSignal(String mode)
+{
+
+    if (mode.compareTo("ON"))
+        digitalWrite(_signalPin[1], HIGH);
+
+    else if (mode.compareTo("OFF"))
+        digitalWrite(_signalPin[1], LOW);
+
+}
+
+int ERO::speedCtrl(void)
+{
+    int result = analogRead(_speedCtrlPin);
+    result = map(result,0,1023,0,100);
+    return(result);
+}
+
+void ERO::forward(int speed){
+    EROmotor.backward(speed);
+}
+
+void ERO::test()
+{
+    forward(100);
+}
