cbc25971af0e2390379dc702411214a718aa4576
syed93uthman <syed_uthman@yahoo.com>
2019-12-04 21:28:08 +0800

    Add module header

---
+++ src/Module.h
@@ -0,0 +1,22 @@
+#ifndef Module
+#define Module
+
+typedef struct GamePadModule{
+    int data;
+
+    bool UP = 0;
+    bool DOWN = 0;
+    bool LEFT = 0;
+    bool RIGHT = 0;
+
+    bool CROSS = 0;
+    bool SQUARE = 0;
+    bool CIRCLE = 0;
+    bool TRIANGLE = 0;
+
+    bool SELECT = 0;
+    bool START = 0;
+
+};
+
+#endif
