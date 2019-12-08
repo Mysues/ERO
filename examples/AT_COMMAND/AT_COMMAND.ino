390af668f18a7fc16da7fb4d09215bb22e1ab3df
syed93uthman <syed_uthman@yahoo.com>
2019-12-08 08:04:43 +0800

    Add AT_COMMAND example

---
+++ examples/AT_COMMAND/AT_COMMAND.ino
@@ -0,0 +1,25 @@
+#include <ERO.h>
+#include <EROBLE.h>
+#include "Hardware.h"
+
+ERO BoBot(motorPin, signalPin, BEEPER, SPEED_CTRL);
+EROBLE ble(RX_PIN, TX_PIN, &GamePad);
+
+void setup()
+{
+  Serial.begin(9600);
+  ble.begin(9600);
+}
+
+void loop()
+{
+  if (ble.available())
+  {
+    Serial.write(ble.read());
+  }
+  
+  if (Serial.available())
+  {
+    ble.write(Serial.read());
+  }
+}
